7c612538e192130bea3123d225f0dd0afd65fcf2
m.sergeeva <m.sergeeva@B206PC0029>
2026-02-20 09:36:02 +0300

    Добавьте файлы проекта.

---
+++ Data/DBConttextApp.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myApp.Models;
+
+namespace myApp.Data
+{
+    public class DBConttextApp: DbContext
+    {
+        DBConttextApp(DbContextOptions<DBConttextApp> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace myApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int IDUser { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string login { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+        [ForeignKey("IdRole")]
+        public int IdRole { get; set; }
+
+        public Role? Role { get; set; }
+    }
+}
