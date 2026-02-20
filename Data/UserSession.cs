39742a32b2b13167b87a658d7ba1a5b13b14bf2e
Mistika-Mystery <m-bazhenova@yandex.ru>
2026-02-20 12:15:41 +0300

    1q2

--- Data/UserSession.cs
+++ Data/UserSession.cs
@@ -3,6 +3,7 @@
     public static class UserSession
     {
         public static int UserId { get; set; } = -1; // Значение по умолчанию, -1 значит, что пользователь не авторизован
+
         public static int RoleId { get; set; } = -1; // Значение по умолчанию, -1 значит, что пользователь не авторизован
 
     }
